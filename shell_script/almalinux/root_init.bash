6e58831f70b8e950504ecb3c2c2beeb676c142b8
karpo <me@karpovan.ru>
2023-06-21 14:07:10 +0300

    root init file

---
+++ shell_script/almalinux/root_init.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+current_dir=$(dirname "$0")
+
+
+"${current_dir}"./bashrc.bash
+"${current_dir}"./fzf_install.bash
+"${current_dir}"./ps1_mod.bash
+"${current_dir}"./neovim.bash
