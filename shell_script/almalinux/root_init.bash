cc7fccf8750125dd6456acd0257ad1a688b3313b
karpo <me@karpovan.ru>
2023-06-21 14:08:48 +0300

    fix

--- shell_script/almalinux/root_init.bash
+++ shell_script/almalinux/root_init.bash
@@ -2,7 +2,7 @@
 current_dir=$(dirname "$0")
 
 
-"${current_dir}"./bashrc.bash
-"${current_dir}"./fzf_install.bash
-"${current_dir}"./ps1_mod.bash
-"${current_dir}"./neovim.bash
+"${current_dir}"/bashrc.bash
+"${current_dir}"/fzf_install.bash
+"${current_dir}"/ps1_mod.bash
+"${current_dir}"/neovim.bash
